bd6c2e555dbaf1f5ff0c8ea16215a20e9960ee31
Cesar Anzures <cesaranz@hotmail.com>
2021-03-03 22:39:33 -0600

    added time functionality

---
+++ main.js
@@ -0,0 +1,35 @@
+// DOM Elements
+const time = document.getElementById('time'),
+    greeting = document.getElementById('greeting'),
+    name = document.getElementById('name'),
+    focus = document.getElementById('focus');
+
+// Show time function
+
+function showTime() {
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes();
+        sec = today.getSeconds();
+
+    // Set AM or PM
+
+    const amPm = hour >= 12 ? 'PM' : 'AM';
+
+    // 12hr format
+
+    hour = hour % 12 || 12;
+
+    // output time
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+
+    setTimeout(showTime, 1000);
+}
+
+// Add Zeros
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+//Run
+showTime();
