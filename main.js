8e7fa740e0478b9223b09538ad97c9fcc8efd538
Cesar Anzures <cesaranz@hotmail.com>
2021-03-04 18:46:01 -0600

    added local storage capicity and added new images

--- main.js
+++ main.js
@@ -2,15 +2,17 @@
 const time = document.getElementById('time'),
     greeting = document.getElementById('greeting'),
     name = document.getElementById('name'),
-    focus = document.getElementById('focus');
+    goal = document.getElementById('goal');
+
+// Options
+const showAmPm = true;
 
 // Show time function
-
 function showTime() {
     let today = new Date(),
         hour = today.getHours(),
         min = today.getMinutes();
-        sec = today.getSeconds();
+    sec = today.getSeconds();
 
     // Set AM or PM
 
@@ -21,7 +23,7 @@ function showTime() {
     hour = hour % 12 || 12;
 
     // output time
-    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
 
     setTimeout(showTime, 1000);
 }
@@ -34,7 +36,7 @@ function addZero(n) {
 // set background and greeting
 function setBgGreet() {
     let today = new Date(),
-    hour = today.getHours();
+        hour = today.getHours();
 
     if (hour < 12) {
         //Morning
@@ -44,6 +46,7 @@ function setBgGreet() {
         //Afternoon
         document.body.style.backgroundImage = "url('../images/afternoon.jpg')";
         greeting.textContent = 'Good Afternoon';
+        
     } else {
         //Evening
         document.body.style.backgroundImage = "url('../images/night.jpg')";
@@ -52,6 +55,59 @@ function setBgGreet() {
     }
 }
 
+// Get name 
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+
+// Set Name
+function setName(e) {
+    if (e.type === 'keypress') {
+        //make sure enter is pressed
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        }
+
+    } else {
+        localStorage.setItem('name', e.target.innerText);
+    }
+}
+
+// Set Goal
+function setGoal(e) {
+    if (e.type === 'keypress') {
+        //make sure enter is pressed
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('goal', e.target.innerText);
+            goal.blur();
+        }
+
+    } else {
+        localStorage.setItem('goal', e.target.innerText);
+    }
+}
+
+// Get goal
+function getgoal() {
+    if (localStorage.getItem('goal') === null) {
+        goal.textContent = '[Enter goal]';
+    } else {
+        goal.textContent = localStorage.getItem('goal');
+    }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+goal.addEventListener('keypress', setGoal);
+goal.addEventListener('blur', setGoal);
+
 //Run
 showTime();
 setBgGreet();
+getName();
+getgoal();
